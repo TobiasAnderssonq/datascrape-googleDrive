66b7fdacdcc5bde5e95c6234d929593324daefe5
Fredrik Niva <fredrik.niva@gmail.com>
2019-11-25 14:36:18 +0100

    lagt till histogram

--- nordnet_dataanalysis.py
+++ nordnet_dataanalysis.py
@@ -6,7 +6,7 @@ import os
 def getData(filename):
     if os.path.isfile(filename):
         df = pd.read_csv(filename)
-    elif googleDrive.checkFileIfPresent != None:
+    elif googleDrive.checkFileIfPresent:
         googleDrive.downloadFromGoogleDrive(filename)
         df = pd.read_csv(filename)
     else:
@@ -18,8 +18,16 @@ def line_chart(df, column_x, column_y, sortbyColumn=None, sortbyValue=None):
         plt.plot(df.column_x, df.column_y)
     else:
         sorted_df = df[df.sortbyColumn == sortbyValue]
-        plt.plot(sorted_df.column_x, df.column_y)
+        plt.plot(sorted_df.column_x, sorted_df.column_y)
+    plt.show()
+
+def hist_chart(df, column_x, column_y, sortbyColumn=None, sortbyValue=None):
+    if sortbyColumn == None:
+        plt.hist(df.column_x, df.column_y)
+    else:
+        sorted_df = df[df.sortbyColumn == sortbyValue]
+        plt.hist(sorted_df.column_x, sorted_df.column_y)
     plt.show()
 
 nordnet_df = getData("nordnetStockData.csv")
-line_chart(nordnet_df, )#VAD DET NU BLIR HÄR
+line_chart(nordnet_df, "Omsatt", "Date", "Aktie", "EQT AB")
