26cc7b7e6f4d7fa0b7d9ededb92d51864e261376
Fredrik Niva <fredrik.niva@gmail.com>
2019-12-08 21:52:46 +0100

    Gjorde små buggfixar. Den plottar nu linjegrafer rätt

--- nordnet_dataanalysis.py
+++ nordnet_dataanalysis.py
@@ -15,19 +15,19 @@ def getData(filename):
 
 def line_chart(df, column_x, column_y, sortbyColumn=None, sortbyValue=None):
     if sortbyColumn == None:
-        plt.plot(df.column_x, df.column_y)
+        plt.plot(df[column_x], df[column_y])
     else:
-        sorted_df = df[df.sortbyColumn == sortbyValue]
-        plt.plot(sorted_df.column_x, sorted_df.column_y)
+        sorted_df = df[df[sortbyColumn] == sortbyValue]
+        plt.plot(sorted_df[column_x], sorted_df[column_y])
     plt.show()
 
 def hist_chart(df, column_x, column_y, sortbyColumn=None, sortbyValue=None):
     if sortbyColumn == None:
-        plt.hist(df.column_x, df.column_y)
+        plt.hist(df[column_x], df[column_y])
     else:
-        sorted_df = df[df.sortbyColumn == sortbyValue]
-        plt.hist(sorted_df.column_x, sorted_df.column_y)
+        sorted_df = df[df[sortbyColumn] == sortbyValue]
+        plt.hist(sorted_df[column_x], sorted_df[column_y])
     plt.show()
 
 nordnet_df = getData("nordnetStockData.csv")
-line_chart(nordnet_df, "Omsatt", "Date", "Aktie", "EQT AB")
+line_chart(nordnet_df, "Date", "Omsättning", "Namn", "Volvo, AB ser. B")
